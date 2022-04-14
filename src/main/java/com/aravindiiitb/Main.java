acd262f040cdb2e50e53fc312eac7a8bd6d4e10e
aravindveluri <aravind.veluri@iiitb.org>
2022-04-14 11:20:19 +0530

    setup intellij workspace

--- src/main/java/com/aravindiiitb/Main.java
+++ src/main/java/com/aravindiiitb/Main.java
@@ -1,4 +1,6 @@
-package com.aravindiiitb;public class Main {
+package com.aravindiiitb;
+
+public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
