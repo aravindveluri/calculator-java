2e8770eae723ad873a71e3f4896f3d363252d41a
aravindveluri <aravind.veluri@iiitb.org>
2022-04-15 02:51:17 +0530

    Added factorial function

--- src/main/java/com/calculator/Calculator.java
+++ src/main/java/com/calculator/Calculator.java
@@ -6,6 +6,16 @@ public class Calculator {
         System.out.println("Computing square root of " + input + "...");
         return Math.sqrt(input);
     }
+    public Long factorial(int input) {
+        System.out.println("Computing factorial of " + input + "...");
+        if (input < 0)
+            return null;
+        long ans = 1;
+        for (int i=0; i<input; i++) {
+            ans *= input - i;
+        }
+        return ans;
+    }
     public static void main(String[] args) {
 
         Calculator calculator = new Calculator();
