f5f2c3d1df5d37b183453b0e64b150012ce8fabe
aravindveluri <aravind.veluri@iiitb.org>
2022-04-15 00:14:50 +0530

    Add square root function

---
+++ src/main/java/com/calculator/Calculator.java
@@ -0,0 +1,14 @@
+package com.calculator;
+
+public class Calculator {
+
+    private double squareRoot(double input) {
+        System.out.println("Computing square root of " + input + "...");
+        return Math.sqrt(input);
+    }
+    public static void main(String[] args) {
+
+        Calculator calculator = new Calculator();
+
+    }
+}
---
+++ src/test/java/com/calculator/CalculatorTest.java
@@ -0,0 +1,10 @@
+package com.calculator;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class CalculatorTest {
+    private static final double error = 1e-15;
+    Calculator calculator = new Calculator();
+
+}
