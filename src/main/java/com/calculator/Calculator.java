42264ebdad8127581f00f8b84ee9d30efc736f11
aravindveluri <aravind.veluri@iiitb.org>
2022-04-16 18:35:15 +0530

    Added logging functionality using log4j

--- src/main/java/com/calculator/Calculator.java
+++ src/main/java/com/calculator/Calculator.java
@@ -1,12 +1,19 @@
 package com.calculator;
 import java.util.Scanner;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class Calculator {
 
+    private static final Logger calculatorLogger = LogManager.getLogger(Calculator.class);
+
     public double squareRoot(double input) {
+        calculatorLogger.info("Computing square root of " + input);
         System.out.println("Computing square root of " + input + "...");
         return Math.sqrt(input);
     }
     public double factorial(int input) {
+        calculatorLogger.info("Computing factorial of " + input);
         System.out.println("Computing factorial of " + input + "...");
         if (input < 0)
             return Double.NaN;
@@ -18,16 +25,19 @@ public class Calculator {
     }
 
     public double naturalLog(double input) {
+        calculatorLogger.info("Computing natural log of " + input);
         System.out.println("Computing natural log of " + input + "...");
         return Math.log(input);
     }
 
     public double power(double input, double exponent) {
+        calculatorLogger.info("Computing " + input + " power " + exponent);
         System.out.println("Computing " + input + " power " + exponent + "...");
         return Math.pow(input, exponent);
     }
 
     private void displayMenu() {
+        calculatorLogger.info("Displaying menu");
         System.out.println("1. Square Root (x)");
         System.out.println("2. Factorial (x) ");
         System.out.println("3. Natural Logarithm (x)");
@@ -36,6 +46,7 @@ public class Calculator {
 
     }
     public static void main(String[] args) {
+        calculatorLogger.info("Initiating Calculator");
 
         Calculator calculator = new Calculator();
         Scanner inputScanner = new Scanner(System.in);
@@ -49,6 +60,8 @@ public class Calculator {
 
             try {
                 int option = inputScanner.nextInt();
+                calculatorLogger.info("User input of " + option);
+                calculatorLogger.info("Option " + option);
                 if (option != 0)
                     System.out.print("Enter your number(s): ");
 
@@ -57,38 +70,51 @@ public class Calculator {
 
                 switch (option) {
                     default:
+                        calculatorLogger.info("Illegal input given");
                         exit = true;
                         System.out.println("Illegal input");
+                        break;
                     case 0:
                         exit = true;
                         break;
                     case 1:
                         arg1 = inputScanner.nextDouble();
+                        calculatorLogger.info("User input of " + arg1);
                         result = calculator.squareRoot(arg1);
                         break;
                     case 2:
                         arg1 = inputScanner.nextDouble();
+                        calculatorLogger.info("User input of " + arg1);
                         result = calculator.factorial((int) arg1);
                         break;
                     case 3:
                         arg1 = inputScanner.nextDouble();
+                        calculatorLogger.info("User input of " + arg1);
                         result = calculator.naturalLog(arg1);
                         break;
                     case 4:
                         arg1 = inputScanner.nextDouble();
+                        calculatorLogger.info("User input of " + arg1);
                         arg2 = inputScanner.nextDouble();
+                        calculatorLogger.info("User input of " + arg2);
                         result = calculator.power(arg1, arg2);
                         break;
                 }
 
-                if (!exit)
+                if (!exit){
+                    calculatorLogger.info("Operation result " + result);
                     System.out.println("Your result is " + result);
+                }
+                else {
+                    calculatorLogger.info("Exiting");
+                }
             }
             catch (Exception e) {
+                calculatorLogger.info("Illegal Input");
                 System.out.println("Illegal input");
                 exit = true;
             }
-
         }
+        calculatorLogger.info("Exited");
     }
 }
